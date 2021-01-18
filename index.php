baba8997a2936e74e7d5afa34686775f90064bfa
Alonzocarlos95 <43510038+Alonzocarlos95@users.noreply.github.com>
2021-01-18 02:09:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Landing Page for CSE-341 Assignments">
+    <title>CSE-341 | Assignments</title>
+    <!--CSS References-->
+    <link rel="stylesheet" href="css/large.css">
+</head>
+<body>
+<header>
+<?php require $_SERVER['DOCUMENT_ROOT'].'/backend_assignments/snippets/header.php'; ?>
+</header>
+<main>
+    <section id="drop">
+    <i id="arrowDirection" class="arrow right"></i>
+    <h2 class="weeks">Week 2</h2>
+    </section>
+    <h3 id="listOfApps"><a href="/backend_assignments/week1/?action=intro-view">Introduction</a></h3>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 3</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 4</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 5</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 6</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 7</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 8</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 9</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 10</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 11</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 12</h2>
+    </section>
+    <section>
+    <i class="arrow right"></i>
+    <h2 class="weeks">Week 13</h2>
+    </section>
+
+</main>
+    <footer>
+    <?php require $_SERVER['DOCUMENT_ROOT'].'/backend_assignments/snippets/footer.php'; ?>
+    </footer>
+    <script src="js/events.js"></script>
+</body>
+</html>
---
+++ introduction.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSE-341 | Introduction</title>
+    <!--CSS References-->
+    <link rel="stylesheet" href="../css/large.css">
+</head>
+<body>
+<header>
+<?php require $_SERVER['DOCUMENT_ROOT'].'/backend_assignments/snippets/header.php'; ?>
+</header>
+<main>
+<h2 class="intro">Hi I'm Carlos Alonzo</h2>
+<section>
+<p class="myIntro">I am from Portoviejo, Ecuador. I have 25 years old. I served the mission in Reynosa - Mexico. I started programming at the age of 17. I love to play piano and read books.</p>
+</section>
+<div class="container">
+<figure>
+<img class="intro-photo" src="/backend_assignments/img/parque.jpg" alt="LDS Missionary">
+<figcaption>
+<span class="photo-desc">Place:</span>
+<p>"Las Vegas" Park, Portoviejo.</p>
+</figcaption>
+</figure>
+</div>
+</main>    
+<footer>
+    <?php require $_SERVER['DOCUMENT_ROOT'].'/backend_assignments/snippets/footer.php'; ?>
+    </footer>
+</body>
+</html>
